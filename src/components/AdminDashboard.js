8dd1bd25c2cbed6c60ce8fddcb155a34d21e6896
Swapin Vidya <swapin@laravelone.in>
2024-10-24 13:37:47 +0530

    first commit

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -1,92 +1,42 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 const AdminDashboard = () => {
+    const [images, setImages] = useState([]);
+
+    // Fetch images when the component mounts
+    useEffect(() => {
+        const fetchImages = async () => {
+            try {
+                const response = await axios.get('http://localhost:8000/api/user-images/1'); // Replace 1 with the actual user ID
+                const { data } = response.data;
+
+                setImages(data);
+            } catch (error) {
+                console.error("Error fetching images:", error);
+            }
+        };
+
+        fetchImages();
+    }, []);
+
     return (
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-            <div>
-                <img
-                    className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
-                    alt=""
-                />
-            </div>
-            <div>
-                <img
-                    className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-                    alt=""
-                />
-            </div>
-            <div>
-                <img
-                    className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
-                    alt=""
-                />
-            </div>
-            <div>
-                <img
-                    className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
-                    alt=""
-                />
-            </div>
-            <div>
-                <img
-                    className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
-                    alt=""
-                />
-            </div>
-            <div>
-                <img
-                    className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
-                    alt=""
-                />
-            </div>
-            <div>
-                <img
-                    className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg"
-                    alt=""
-                />
-            </div>
-            <div>
-                <img
-                    className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg"
-                    alt=""
-                />
-            </div>
-            <div>
-                <img
-                    className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg"
-                    alt=""
-                />
-            </div>
-            <div>
-                <img
-                    className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg"
-                    alt=""
-                />
-            </div>
-            <div>
-                <img
-                    className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg"
-                    alt=""
-                />
-            </div>
-            <div>
-                <img
-                    className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
-                    alt=""
-                />
-            </div>
+            {images.length > 0 ? (
+                images.map((image) => (
+                    <div key={image.id}>
+                        <img
+                            className="h-auto max-w-full rounded-lg"
+                            src={image.url}
+                            alt={image.filename}
+                        />
+                    </div>
+                ))
+            ) : (
+                <p>No images available</p>
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
